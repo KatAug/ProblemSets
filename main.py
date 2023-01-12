a360bc9a3e940d5fa07a4661c7c0022fc2010414
Katrina Augustine <katrinaaugustine2022@gmail.com>
2023-01-12 17:46:58 -0600

    Solved problems 3-5 in problem one

--- main.py
+++ main.py
@@ -9,5 +9,39 @@ print(result)
 
 num_list = [1, 2, 3, 4, 5, 6]
 i = 4
+new_list = []
 result = problems_one.list_value_checker(num_list, i)
+print(new_list) 
+
+list_one = ["David", "Katrina", "Keisha", "Nick", "Jaxson"]
+list_two = ["Matthew", "Brianna", "Samantha", "Catherine", "Nick"]
+result = problems_one.list_value_comparison(list_one, list_two)
+print(result)
+
+#Do I need to copy the dictionary onto this page? 
+person = {
+    "name": "Timmy Thomas",
+    "age": 5,
+    "interests": {
+        "favorite_book": "Where The Sidewalk Ends",
+        "favorite_movie": "Star Wars",
+        "favorite_color": "Red"
+    }
+}
+sample_dictionary = (person['interests']['favorite_color'])
+result = problems_one.get_value_of_favorite_color(sample_dictionary)
+print(sample_dictionary)
+
+address = {
+	"street": "123 Sesame Street",
+	"city": "Some Town",
+	"state": "Some State",
+	"zip_code": 12345
+}
+dictionary = (address)
+result = problems_one.dictionary_printer(dictionary)
+#print(dictionary["street"])
+#print(dictionary["city"])
+#print(dictionary["state"])
+#print(dictionary["zip_code"])
 
--- problems_one.py
+++ problems_one.py
@@ -12,7 +12,8 @@ def reverse_list(to_reverse):
     reversed(to_reverse)    #just reverses
     result = reversed(to_reverse)  #captures the reversed list
     list_result = list(result) #puts the reversed list into a list 
-    return(list_result)
+    return list_result
+
 
 # Problem 2
 
@@ -28,16 +29,14 @@ Example Output: [4, 5]
 """
 num_list = [1, 2, 3, 4, 5, 6]
 i = 4
-new_list = []
+new_list = []      #it only prints [], "Why?"
 def list_value_checker(num_list, i):
     while i < len(num_list):
-        result = (num_list[i])
         i = i + 1
-        new_list.append(result)
-        print(new_list)
-
-#list_value_checker(num_list, i) 
-
+        new_list.append(i)
+        return new_list
+        
+                          
 # Problem 3
 
 """
@@ -52,9 +51,11 @@ Example Output: "Mike"
 list_one = ["David", "Katrina", "Keisha", "Nick", "Jaxson"]
 list_two = ["Matthew", "Brianna", "Samantha", "Catherine", "Nick"]
 def list_value_comparison(list_one, list_two):
-   pass 
+    for n in list_one:
+        for x in list_two:
+            if n == x:
+                return n
 
-list_value_comparison(list_one, list_two)
 
 # Problem 4
 
@@ -74,10 +75,20 @@ person = {
 
 Expected Output: "Red"
 """
+person = {
+    "name": "Timmy Thomas",
+    "age": 5,
+    "interests": {
+        "favorite_book": "Where The Sidewalk Ends",
+        "favorite_movie": "Star Wars",
+        "favorite_color": "Red"
+    }
+}
 
-
+sample_dictionary = (person['interests']['favorite_color'])
 def get_value_of_favorite_color(sample_dictionary):
-    pass
+    return sample_dictionary
+
 
 # Problem 5
 
@@ -103,9 +114,25 @@ Example Output
 # Example: "Zip Code - 12345"
 """
 
+address = {
+	"street": "123 Sesame Street",
+	"city": "Some Town",
+	"state": "Some State",
+	"zip_code": 12345
+}
 
+keys = address.keys()
+values = address.values()
+dictionary = (address)
 def dictionary_printer(dictionary):
-    pass
+    for key, value in dictionary.items():
+        print(f"{key}: {value}")
+    
+#dictionary_printer(dictionary)
+#print(dictionary["street"])
+#print(dictionary["city"])
+#print(dictionary["state"])
+#print(dictionary["zip_code"])
 
 
 # Problem 6
@@ -120,6 +147,6 @@ Example Input: [1, 1, 2, 2, 2, 3]
 Example Output: {"1": 2, "2": 3, "3": 1}
 """
 
-
+list_of_numbers = [1,1,3,4,4,5,5,6,7,7,7]
 def list_numbers_to_dictionary(list_of_numbers):
     pass
